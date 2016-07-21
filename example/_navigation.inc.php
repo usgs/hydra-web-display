0bf5f2a9bc42fd850cc76a1f25f98e0b8f72fc38
ehunter-usgs <ehunter@usgs.gov>
2016-07-21 11:56:32 -0600

    Add tests and example page for MagnitudeSummaryView

--- example/_navigation.inc.php
+++ example/_navigation.inc.php
@@ -2,12 +2,14 @@
 
 echo navItem('/example.php', 'Examples');
 
+
 print navGroup('Event',
     navItem('/EventSummaryViewExample.php', 'EventSummaryView')
   );
 
 print navGroup('Magnitude',
-    navItem('/MagnitudeCollectionTableExample.php', 'MagnitudeCollectionTable')
+    navItem('/MagnitudeCollectionTableExample.php', 'MagnitudeCollectionTable') .
+    navItem('/MagnitudeSummaryViewExample.php', 'Magnitude Summary View')
   );
 
 ?>
