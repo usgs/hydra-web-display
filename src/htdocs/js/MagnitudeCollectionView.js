88ec83e98acf6b742b1d9cde2cf638edd163b8fd
Eric Martinez <emartinez@usgs.gov>
2016-07-21 10:27:43 -0600

    Switched to use a CollectionTable since that is the intended implementation.

--- src/htdocs/js/MagnitudeCollectionView.js
+++ src/htdocs/js/MagnitudeCollectionView.js
@@ -3,27 +3,69 @@
 
 var Formatter = require('Formatter'),
     Util = require('util/Util'),
-    CollectionView = require('mvc/CollectionView');
+    CollectionTable = require('mvc/CollectionTable');
 
 
 var _DEFAULTS;
 
-_DEFAULTS = {};
+_DEFAULTS = {
+  className: 'magnitude-collection-view',
+  clickToSelect: true,
+  columns: [
+    {
+      className: 'id',
+      title: 'ID',
+      format: function (item) {
+        return item.id;
+      },
+      header: true
+    },
+    {
+      className: 'author',
+      title: 'Author',
+      format: function (item) {
+        return item.author;
+      }
+    },
+    {
+      className: 'installation',
+      title: 'Installation',
+      format: function (item) {
+        return item.installation;
+      }
+    },
+    {
+      className: 'type',
+      title: 'Type',
+      format: function (item) {
+        return item.type;
+      }
+    },
+    {
+      className: 'value',
+      title: 'Value',
+      format: function (item) {
+        return item.value;
+      }
+    }
+  ]
+};
 
 
 var MagnitudeCollectionView = function (options) {
-
   var _this,
       _initialize,
 
       _formatter;
 
+
   options = Util.extend({}, _DEFAULTS, options);
-  _this = CollectionView(options);
+  _this = CollectionTable(options);
 
   _initialize = function (options) {
     _formatter = options.formatter || Formatter();
   };
+
 
   _this.destroy = Util.compose(function () {
     _formatter = null;
@@ -32,9 +74,6 @@ var MagnitudeCollectionView = function (options) {
     _this = null;
   }, _this.destroy);
 
-  _this.render = function () {
-    _this.el.innerHTML = 'TODO:: MagnitudeCollectionView';
-  };
 
   _initialize(options);
   options = null;
