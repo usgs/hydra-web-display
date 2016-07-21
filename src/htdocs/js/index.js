3e30088fad6b91d6dd328b214d69d898fcfc4fe5
Eric Martinez <emartinez@usgs.gov>
2016-07-21 10:27:11 -0600

    Pointing view to static JSON file for dev until WS is ready.

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -5,5 +5,6 @@ var EventSearchView = require('EventSearchView');
 
 
 EventSearchView({
-  el: document.querySelector('#event-search-view')
+  el: document.querySelector('#event-search-view'),
+  eventWsUrl: '/event.json'
 });
