6a7244c3c2c686fefad77e915609189b56aa5ece
ehunter-usgs <ehunter@usgs.gov>
2016-07-22 12:48:47 -0600

    Create table with moment-tensor beach ball in MagnitudeSummaryView

--- example/MagnitudeSummaryViewExample.js
+++ example/MagnitudeSummaryViewExample.js
@@ -1,16 +1,36 @@
 'use strict';
 
-var MagnitudeSummaryView = require('MagnitudeSummaryView'),
+var Collection = require('mvc/Collection'),
+    MagnitudeSummaryView = require('MagnitudeSummaryView'),
     Model = require('mvc/Model'),
     Xhr = require('util/Xhr');
 
 Xhr.ajax({
-  url: 'event.json',
+  url: 'magnitude.json',
   success: function (data) {
-    var magnitudeCollectionView;
+    var magnitudeCollectionView,
+        magnitudes;
+
+    magnitudes = [
+      Model({
+        'id': '50003UWR/wphase_module/NEIC/Mww',
+        'author': 'wphase_module',
+        'installation': 'NEIC',
+        'type': 'Mww',
+        'value': 5.768
+      }),
+      Model({
+        'id': '50003UWR/author/installation/Mqa',
+        'author': 'author',
+        'installation': 'installation',
+        'type': 'Mww',
+        'value': 5.012
+      }),
+    ];
 
     magnitudeCollectionView = MagnitudeSummaryView({
       el: document.querySelector('#magnitude-summary-view-example'),
+      collection: Collection(magnitudes),
       model: Model(data)
     });
     magnitudeCollectionView.render();
