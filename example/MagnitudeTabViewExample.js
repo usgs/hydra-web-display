1f63dc1fe38b534e5f3bd85646e628ded834e351
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-07-26 13:53:48 -0600

    removed code that was not needed

--- example/MagnitudeTabViewExample.js
+++ example/MagnitudeTabViewExample.js
@@ -9,23 +9,3 @@ var magnitudeTabView = MagnitudeTabView({
 });
 
 magnitudeTabView.render();
-// Xhr.ajax({
-//   url: 'event.json',
-//   success: function (data) {
-//     var model;
-//
-//     model = EventModel.fromFeature(data);
-//
-//     MagnitudeTabView({
-//       el: document.querySelector('#magnitude-tab-view-example'),
-//       model: model
-//     }).render();
-//   },
-//   error: function () {
-//     document.querySelector('#magnitude-tab-view-example').innerHTML = [
-//       '<p class="alert error">',
-//         'Failed to create a event summary view.',
-//       '</p>'
-//     ].join('');
-//   }
-// });
