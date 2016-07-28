091e22c708f43bf197741b6b82ea3559ed747107
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-07-28 14:40:21 -0600

    removed code that was no longer needed, changed private var to public

--- src/htdocs/js/MagnitudeTabView.js
+++ src/htdocs/js/MagnitudeTabView.js
@@ -13,50 +13,47 @@ _DEFAULTS = {};
 
 
 var MagnitudeTabView = function (options) {
-
   var _this,
-      _initialize,
-
-      _magnitudeSummaryView,
-      _tabList;
+      _initialize;
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = View(options);
 
 
   _initialize = function () {
-    var model;
-
-    model = options.model || _this.model;
-
     _this.el.classList.add('magnitude-tab-view');
-    _magnitudeSummaryView = MagnitudeSummaryView({
-        model: model
+    _this.magnitudeSummaryView = MagnitudeSummaryView({
+        model: _this.model
     });
   };
 
-  _this.destroy = Util.compose(function () {
-    _tabList = null;
 
+  /**
+   * Destroy all the things.
+   */
+  _this.destroy = Util.compose(function () {
     _initialize = null;
     _this = null;
   }, _this.destroy);
 
-  // Add new tabs here
+  /**
+   * Add new tabs here.
+   */
   _this.addTabs = function () {
-    _tabList.addTab({
+    _this.tabList.addTab({
       title: 'Magnitude Summary',
-      content: _magnitudeSummaryView.el,
+      content: _this.magnitudeSummaryView.el,
       onSelect: function () {
-        _magnitudeSummaryView.render();
+        _this.magnitudeSummaryView.render();
       }
     });
   };
 
+  /**
+   * Creates tablist
+   */
   _this.render = function () {
-    _this.el.innerHTML = '';
-
-    _tabList = new TabList({
+    _this.tabList = new TabList({
       el: document.querySelector('.magnitude-tab-view'),
       tabPosition: 'top',
       tabs: []
