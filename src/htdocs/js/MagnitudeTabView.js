b20bb0949439a4874800e2fac338142795a9f807
Eric Martinez <emartinez@usgs.gov>
2016-08-01 10:53:38 -0600

    Refactored to be more extensible in the future.

--- src/htdocs/js/MagnitudeTabView.js
+++ src/htdocs/js/MagnitudeTabView.js
@@ -9,22 +9,119 @@ var MagnitudeSummaryView = require('MagnitudeSummaryView'),
 
 var _DEFAULTS;
 
-_DEFAULTS = {};
+_DEFAULTS = {
+  // Ordered list of configured tabs to add automatically
+  tabs: [
+    // Summary
+    {
+      factory: MagnitudeSummaryView,
+      id: 'summary',
+      title: 'Summary',
+    }
+  ]
+};
 
 
 var MagnitudeTabView = function (options) {
   var _this,
-      _initialize;
+      _initialize,
+
+      _createTabList;
+
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = View(options);
 
+  _initialize = function (options) {
+    _this.el.innerHTML = '<div class="magnitude-tab-view-tabs"></div>';
 
-  _initialize = function () {
-    _this.el.classList.add('magnitude-tab-view');
-    _this.magnitudeSummaryView = MagnitudeSummaryView({
-        model: _this.model
+    _this.views = {};
+    _this.tabs = {};
+
+    // Create an empty tab list to add tabs to
+    _this.tabList = TabList({
+      el: _this.el.querySelector('.magnitude-tab-view-tabs'),
+      tabs: []
     });
+
+    // Add all configured tabs
+    options.tabs.forEach(function (params) {
+      _this._addTab(Util.extend({tabList: _this.tabList}, params));
+    });
+  };
+
+
+  /**
+   * Creates (or recreates) a specific view/tab based on the given configuration
+   * options.
+   *
+   * @param params {Object}
+   *     params for creating the tab/view.
+   * @param params.factory {Function}
+   *     The factory method to call in order to create a new view.
+   * @param params.id {String}
+   *     The unique ID for the tab/view to be created.
+   * @param params.options {Object}
+   *     Configuration params to provide to the `factory`. These
+   *     will be extended with {model: _this.model}.
+   * @param params.tabList {TabList}
+   *     The tab list to which the tab will be added.
+   * @param params.title {String}
+   *     The string to use as a title in the tab.
+   */
+  _this._addTab = function (params) {
+    var factory,
+        id,
+        options,
+        tab,
+        tabList,
+        title,
+        view;
+
+    id = params.id || 'id';
+    tabList = params.tabList || {addTab: function () {}};
+    title = params.title || 'Title';
+    factory = params.factory || function () {};
+    options = params.options || {};
+
+    // Create or recreate the view
+    view = _this.views[id];
+    if (view && typeof view === 'function') {
+      view.destroy();
+      _this.views[id] = null;
+    }
+
+    view = factory(Util.extend({model: _this.model}, options));
+    _this.views[id] = view;
+
+    // Create the tab or use existing tab. TabList does not currently support
+    // removing a specific tab in order to replace it, but by holding a
+    // reference to the created tab, we can augment it to swap out content
+    // if needed.
+    tab = _this.tabs[id];
+    if (tab) {
+      tab.options.content = view.el;
+
+      if (tab.tabEl.classList.contains('tablist-tab-selected')) {
+        // Tab is selected, must update panel content
+        Util.empty(tab.panelEl);
+        tab.panelEl.appendChild(view.el);
+      } else {
+        // Force next select to look at new view
+        tab.contentReady = false;
+      }
+    } else {
+      tab = {
+        title: title,
+        content: view.el,
+        onSelect: function () {
+          _this.views[id].render();
+        }
+      };
+
+      tab = tabList.addTab(tab);
+      _this.tabs[id] = tab;
+    }
   };
 
 
@@ -32,38 +129,37 @@ var MagnitudeTabView = function (options) {
    * Destroy all the things.
    */
   _this.destroy = Util.compose(function () {
+    if (_this === null) {
+      return;
+    }
+
+    // Destroy sub-views first
+    Object.keys(_this.views).forEach(function (id) {
+      var view;
+
+      view = _this.views[id];
+      if (view && typeof view.destroy === 'function') {
+        view.destroy();
+      }
+      _this.views[id] = null;
+    });
+
+
+    // Now destroy TabList itself
+    if (_this.tabList && typeof _this.tabList.destroy === 'function') {
+      _this.tabList.destroy();
+    }
+
+
+    // Clean up remaining resources
+    _createTabList = null;
+
     _initialize = null;
     _this = null;
   }, _this.destroy);
 
-  /**
-   * Add new tabs here.
-   */
-  _this.addTabs = function () {
-    _this.tabList.addTab({
-      title: 'Magnitude Summary',
-      content: _this.magnitudeSummaryView.el,
-      onSelect: function () {
-        _this.magnitudeSummaryView.render();
-      }
-    });
-  };
 
-  /**
-   * Creates tablist
-   */
-  _this.render = function () {
-    _this.tabList = new TabList({
-      el: document.querySelector('.magnitude-tab-view'),
-      tabPosition: 'top',
-      tabs: []
-    });
-
-    _this.addTabs();
-  };
-
-
-  _initialize();
+  _initialize(options);
   options = null;
   return _this;
 };
